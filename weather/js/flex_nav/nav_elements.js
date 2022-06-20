194e16e94243ca9f6abbd5a3839d9a4420291afb
jamesjavanyc <1931736857@gmail.com>
2022-06-20 13:54:15 -0400

    js

--- weather/js/flex_nav/nav_elements.js
+++ weather/js/flex_nav/nav_elements.js
@@ -13,9 +13,9 @@ const nav_elements_initer = ()=>{
             // console.log(this.nav_list)
             this.lp_switch = this.nav_list.children[this.nav_list.children.length-1];
             // console.log(this.lp_switch)
-            if (window.innerWidth>1160){
-                nav_elements.lp_switch.children[0].setAttribute('style', 'display:none;');
-            }
+            // if (window.innerWidth>1160){
+            //     nav_elements.lp_switch.children[0].setAttribute('style', 'display:none;');
+            // }
             this.nav_list.children[parseInt(getComputedStyle(document.documentElement).getPropertyValue('--page-id').trim())-1].children[0].setAttribute('style','color:yellow;');
             this.mb_switch_icon= this.mb_switch.childNodes[1];
             // console.log(this.mb_switch)
