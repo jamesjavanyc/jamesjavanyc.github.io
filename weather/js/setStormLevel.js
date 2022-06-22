06c0bacfa3433626a040a102def6e06b979dac1a
jamesjavanyc <1931736857@gmail.com>
2022-06-21 22:49:08 -0400

    finaldebug

--- weather/js/setStormLevel.js
+++ weather/js/setStormLevel.js
@@ -1,7 +1,11 @@
-const adjustRating= ()=> {
-    console.log(document.getElementById("str_range"));
-    document.getElementById("str_range").innerHTML = document.getElementById('storm_rate_input').getAttribute('value');
+const adjustRating= (value)=> {
+	console.log(document.getElementById('storm_rate_input').value);
+    document.getElementById("str_range").innerHTML = value;
 }
+
+// function adjustRating(rating) {
+//     document.getElementById("ratingvalue").innerHTML = rating;
+// }
 
 function selectResponse() {
 	const s = document.querySelector('#storm_rate_input');
