bc06fa7f1507b30f7d10b1f58e36052d789000c7
jamesjavanyc <1931736857@gmail.com>
2022-07-05 17:53:55 -0400

    1

--- scoots/js/fill_rentals.js
+++ scoots/js/fill_rentals.js
@@ -1,20 +1,24 @@
 let motors = Array.from(document.getElementsByClassName('motor'));
 const fill_motor = async()=>{
-    let datasetPromise = fetch('https://jamesjavanyc.github.io/scoots/data/price.json');
-    let dataset = await datasetPromise;
-    dataset = await dataset.json();
-    let i = 0;
-    motors.forEach((motor)=>{
-        motor.children[0].innerText = dataset[i].name;
-        motor.children[1].children[0].src ='./source/mt'+ (i+1)+'.jpg';
-        let ul = motor.children[1].children[1];
-        ul.children[0].children[1].children[0].innerHTML = dataset[i].max;
-        ul.children[2].children[1].children[0].innerHTML = dataset[i].rhalf;
-        ul.children[3].children[1].children[0].innerHTML = dataset[i].rfull;
-        ul.children[5].children[1].children[0].innerHTML = dataset[i].whalf;
-        ul.children[6].children[1].children[0].innerHTML = dataset[i].wfull;
-        i++;
-    })
+    try{
+        let datasetPromise = fetch('https://jamesjavanyc.github.io/scoots/data/price.json');
+        let dataset = await datasetPromise;
+        dataset = await dataset.json();
+        let i = 0;
+        motors.forEach((motor)=>{
+            motor.children[0].innerText = dataset[i].name;
+            motor.children[1].children[0].src ='./source/mt'+ (i+1)+'.jpg';
+            let ul = motor.children[1].children[1];
+            ul.children[0].children[1].children[0].innerHTML = dataset[i].max;
+            ul.children[2].children[1].children[0].innerHTML = dataset[i].rhalf;
+            ul.children[3].children[1].children[0].innerHTML = dataset[i].rfull;
+            ul.children[5].children[1].children[0].innerHTML = dataset[i].whalf;
+            ul.children[6].children[1].children[0].innerHTML = dataset[i].wfull;
+            i++;
+        })
+    }catch(error){
+        console.err(error);
+    }
 }
 
 export {fill_motor}
