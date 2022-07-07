f5c51ca650bdf8090b7997e50cc592034d29e9d9
jamesjavanyc <1931736857@gmail.com>
2022-07-07 15:19:57 -0400

    1

--- scoots/js/init.js
+++ scoots/js/init.js
@@ -21,6 +21,7 @@ if(page_nav_id == 1 ){
 initdata.forEach(function async(callback){
     // console.log("initializing")
     let res = callback();
+    console.log('call back called')
     if(typeof(res)==='object'){
         res.then(()=>{
             console.log('fetched')
