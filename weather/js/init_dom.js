6dcb4ed625479663aed0f3238d7b6fb5aa469986
jamesjavanyc <1931736857@gmail.com>
2022-07-09 00:24:36 -0400

    1

--- weather/js/init_dom.js
+++ weather/js/init_dom.js
@@ -6,16 +6,16 @@ let main = body.children[2];
 
 
 let page_nav_id;
-setTimeout(()=>{
-    while(!(page_nav_id in [1,2,3,4,5,6])){
-        try{
-            page_nav_id = parseInt(getComputedStyle(header).getPropertyValue('--page-id').trim());
-            // console.log('get ID');
-        }catch(e){
-            console.error(e)
-        }
-    }
-},2000)
 
+// while(!(page_nav_id in [1,2,3,4,5,6])){
+//     try{
+//         page_nav_id = parseInt(getComputedStyle(header).getPropertyValue('--page-id').trim());
+//         // console.log('get ID');
+//     }catch(e){
+//         console.error(e)
+//     }
+// }
+
+page_nav_id = header.children[header.children.length-1].getAttribute('data-id');
 export {body,header, banner, main, page_nav_id};
 
