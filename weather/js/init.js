3c8839fac03aba7be8dc3501bfa745eaa9d502f9
jamesjavanyc <1931736857@gmail.com>
2022-07-08 23:56:55 -0400

    1

--- weather/js/init.js
+++ weather/js/init.js
@@ -1,41 +1,40 @@
 import { setCurrentDate } from "./footer.js";
-import {nav_exp, getNavOnChange} from "./flex_nav/flex_nav.js"
-import {setBanner} from './header.js'
+import { nav_exp, getNavOnChange } from "./flex_nav/flex_nav.js"
+import { setBanner } from './header.js'
 // import {setWindChill} from './windchill.js'
-import {page_nav_id} from './init_dom.js'
-import {lazyloadImage} from './lazyload.js'
-import {setCityWeather} from './homepage_city_weather.js'
-import {setWeatherData} from './set_weather_data.js'
-import {setEvent} from './set_event.js'
+import { page_nav_id } from './init_dom.js'
+import { lazyloadImage } from './lazyload.js'
+import { setCityWeather } from './homepage_city_weather.js'
+import { setWeatherData } from './set_weather_data.js'
+import { setEvent } from './set_event.js'
 
-const init_js = ()=>{
+const init_js = () => {
     let initdata = new Array();
     initdata.push(nav_exp);
-    if(page_nav_id == 2 ||page_nav_id == 3||page_nav_id == 4 ){
+    if (page_nav_id == 2 || page_nav_id == 3 || page_nav_id == 4) {
         // console.log(2)
         initdata.push(setCurrentDate);
         initdata.push(setBanner);
         initdata.push(setWeatherData);
         initdata.push(setEvent);
-    }else if(page_nav_id == 5){
+    } else if (page_nav_id == 5) {
         // console.log(5)
         initdata.push(setCurrentDate);
-    }else if(page_nav_id == 6){
+    } else if (page_nav_id == 6) {
         // console.log(6)
         initdata.push(lazyloadImage);
-    }else if(page_nav_id == 1){
+    } else if (page_nav_id == 1) {
         initdata.push(setCityWeather);
         // console.log(1)
     }
-    
-    for (const callback of initdata) {
-        try{
-            await callback();
-        }catch(e){
-            console.error(e);
+    async function init(initdata){
+        for(let i = 0; i < initdata.length; i++){
+            await initdata[i]();
         }
     }
-    
+    init(initdata);
+
+
     // initdata.forEach(async function (callback){
     //     try{
     //         await callback();
@@ -45,14 +44,14 @@ const init_js = ()=>{
     //     // console.log(callback);
     // });
     // moniter on window size
-    window.onresize = async function() { 
-        var initer =  getNavOnChange();
+    window.onresize = async function () {
+        var initer = getNavOnChange();
         initer();
     }
 }
 
 document.addEventListener('DOMContentLoaded', init_js)
 
-export {init_js}
+export { init_js }
 
 
