ed2bcb44ca3e1f2ad0bc588b48e43cb70baf1eca
jamesjavanyc <1931736857@gmail.com>
2022-07-08 21:55:53 -0400

    1

--- weather/js/init_dom.js
+++ weather/js/init_dom.js
@@ -5,8 +5,11 @@ let main = body.children[2];
 // console.log(main)
 
 
-let page_nav_id = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--page-id').trim());
-// console.log(page_nav_id)
+let page_nav_id;
+while(Object.is(page_nav_id, NaN)){
+    page_nav_id = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--page-id').trim());
+}
+console.log(page_nav_id)
 
 export {body,header, banner, main, page_nav_id};
 
