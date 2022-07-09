bee89d7650a0e18c40d5f9ce0b2f567257755c2f
jamesjavanyc <1931736857@gmail.com>
2022-07-08 23:16:16 -0400

    1

--- weather/js/init.js
+++ weather/js/init.js
@@ -8,39 +8,44 @@ import {setCityWeather} from './homepage_city_weather.js'
 // import {setWeatherData} from './set_weather_data.js'
 import {setEvent} from './set_event.js'
 
-
-let initdata = new Array();
-initdata.push(nav_exp);
-if(page_nav_id == 2 ||page_nav_id == 3||page_nav_id == 4 ){
-    // console.log(2)
-    initdata.push(setCurrentDate);
-    initdata.push(setBanner);
-    // initdata.push(setWeatherData);
-    initdata.push(setEvent);
-}else if(page_nav_id == 5){
-    // console.log(5)
-    initdata.push(setCurrentDate);
-}else if(page_nav_id == 6){
-    // console.log(6)
-    initdata.push(lazyloadImage);
-}else if(page_nav_id == 1){
-    initdata.push(setCityWeather);
-    // console.log(1)
-}
-
-
-initdata.forEach(async function (callback){
-    try{
-        await callback();
-    }catch(e){
-        console.error(e);
+const init_js = ()=>{
+    let initdata = new Array();
+    initdata.push(nav_exp);
+    if(page_nav_id == 2 ||page_nav_id == 3||page_nav_id == 4 ){
+        // console.log(2)
+        initdata.push(setCurrentDate);
+        initdata.push(setBanner);
+        // initdata.push(setWeatherData);
+        initdata.push(setEvent);
+    }else if(page_nav_id == 5){
+        // console.log(5)
+        initdata.push(setCurrentDate);
+    }else if(page_nav_id == 6){
+        // console.log(6)
+        initdata.push(lazyloadImage);
+    }else if(page_nav_id == 1){
+        initdata.push(setCityWeather);
+        // console.log(1)
     }
-    // console.log(callback);
-});
-
-// moniter on window size
-window.onresize = async function() { 
-    var initer =  getNavOnChange();
-    initer();
+    
+    
+    initdata.forEach(async function (callback){
+        try{
+            await callback();
+        }catch(e){
+            console.error(e);
+        }
+        // console.log(callback);
+    });
+    // moniter on window size
+    window.onresize = async function() { 
+        var initer =  getNavOnChange();
+        initer();
+    }
 }
+
+window.addEventListener('DOMContentLoaded', init_js)
+
+export {init_js}
+
 
